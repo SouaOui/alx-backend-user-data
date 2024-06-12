60c3c61c2c9a1453f4ea9107bed6e29f6490e89d
SouaOui <your.email@example.com>
2024-06-12 16:54:13 +0100

    User authentication service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,26 +1,19 @@
-from sqlalchemy import create_engine
+#!/usr/bin/env python3
+"""doc doc doc.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-Base = declarative_base()
 
-DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
 class User(Base):
+    """doc doc doc
     """
-    create the User class for the users table in the database
-    """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
-    def __init__(self, email: str, hashed_password: str):
-        self.email = email
-        self.hashed_password = hashed_password
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
