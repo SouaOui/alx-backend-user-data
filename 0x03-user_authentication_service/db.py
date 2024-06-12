b3204694c9f339eeb61be9309f5992f403c247c7
SouaOui <your.email@example.com>
2024-06-12 16:50:00 +0100

    User authentication service

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,57 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        """
+        add a user to the database
+        """
+        user = User(email, hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """find user by
+        """
+        if not kwargs:
+            raise InvalidRequestError("invalid")
+        session = self._session
+        query = session.query(User)
+        for column, value in kwargs.items():
+            column_att = getattr(User, column)
+            query = query.filter(column_att == value)
+        try:
+            return query.first()
+        except BaseException:
+            raise NoResultFound("Not Found")
+        pass
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,26 @@
+from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
-import sqlalchemy
-
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
+
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class User(Base):
+    """
+    create the User class for the users table in the database
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(250))
-    hashed_password = Column(String(250))
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
 
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-            self.name, self.fullname, self.nickname)
+    def __init__(self, email: str, hashed_password: str):
+        self.email = email
+        self.hashed_password = hashed_password
